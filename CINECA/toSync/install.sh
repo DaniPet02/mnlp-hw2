0e0185279eec43f1063f333fb6e3df99df09636f
Andrea Pizzi <pizzi.1995517@studenti.uniroma1.it>
2025-06-07 20:11:06 +0200

    make install.sh more smart
    Signed-off-by: Andrea Pizzi <pizzi.1995517@studenti.uniroma1.it>

--- CINECA/toSync/install.sh
+++ CINECA/toSync/install.sh
@@ -8,36 +8,40 @@ module load git
 ENV_NAME="MNLP"
 TOKEN_HF="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"
 # Delete old env
-if [ -d "$ENV_NAME" ]; then
-    echo "remove old env $ENV"
-    rm -rf "$ENV_NAME"
+if [ ! -d "$ENV_NAME" ]; then
+    echo "===[create MNLP]==="
+    python3 -m venv MNLP
+
+    # activating the new virtualenv
+    source MNLP/bin/activate
+
+    # upgrade pip
+    python3 -m pip install --upgrade pip
+
+    # installing whatever you need (e.g matplotlib)
+    pip3 install numpy wandb datasets 
+    pip3 install -e ".[torch,metrics]" --no-build-isolation
+    # hugging-face tools
+    pip3 install -U "huggingface_hub[cli]"
+    python3 -c "from huggingface_hub import login; login(token='$TOKEN_HF')"
+
+
+    # LLMA factory dependencies
+
+    
 fi
 
-python3 -m venv MNLP
+if [ ! -d "LLaMA-Factory" ]; then 
+    git clone --depth 1 https://github.com/hiyouga/LLaMA-Factory.git
 
-# activating the new virtualenv
-source MNLP/bin/activate
-
-# upgrade pip
-python3 -m pip install --upgrade pip
-
-# installing whatever you need (e.g matplotlib)
-pip3 install scipy numpy nltk pandas pytest
-pip3 install matplotlib wandb
-pip3 install seaborn
-pip3 install datasets
-pip3 install -e ".[torch,metrics]" --no-build-isolation
-# hugging-face tools
-pip3 install -U "huggingface_hub[cli]"
-python3 -c "from huggingface_hub import login; login(token='$TOKEN_HF')"
-
-
-# LLMA factory dependencies
-
-git clone --depth 1 https://github.com/hiyouga/LLaMA-Factory.git
-mv config.yaml LLaMA-Factory
+# configure LLMA-factory
+cp config.yaml LLaMA-Factory
 cat dataset_config.json > LLaMA-Factory/data/dataset_info.json
+
 # Enter in LLMA-factory folder
 cd LLaMA-Factory
+
+
+
 
 
